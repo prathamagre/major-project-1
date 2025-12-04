9188badd91b787538c6dbb3e715bd2edae5587b7
prathamagre <prathamvaishu@gmail.com>
2025-12-04 12:12:13 +0530

    changes added in navbar

--- DARA-frontend/src/components/Navbar/Navbar.jsx
+++ DARA-frontend/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,10 @@ import "./Navbar.css"
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">D.A.R.A.</div>
+      <div className="logo">
+        <p className="logo-head">D.A.R.A.</p>
+        <p className="logo-txt">Data Analysis and RESTful APIs</p>
+      </div>
 
       <ul className="nav-links">
         <li><a href="/">Home</a></li>
